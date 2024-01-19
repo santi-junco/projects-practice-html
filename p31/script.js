ffa3635130bf4efb3a4e0abb6eb9e06df9788e62
Santiago Junco <77699351+santi-junco@users.noreply.github.com>
2024-01-19 11:12:08 -0300

    Add project 31

---
+++ p31/script.js
@@ -0,0 +1,17 @@
+const button = document.querySelectorAll("a");
+
+button.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
+    let ripples = document.createElement("span");
+
+    ripples.style.letf = x + "px";
+    ripples.style.top = y + "px";
+    this.appendChild(ripples);
+
+    setTimeout(() => {
+      ripples.remove();
+    }, 1000);
+  });
+});
